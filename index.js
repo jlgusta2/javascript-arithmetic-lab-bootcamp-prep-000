7f08efbecefec5d94a89ec1c34651214291eb262
jennifer gustavson <jengustavson@gmail.com>
2018-05-23 16:47:48 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,60 @@
+function add(x, y) {
+  return x + y
+}
+add(1, 80)
+
+function subtract(x, y) {
+  return x - y
+}
+subtract(60, 40)
+
+function multiply(x, y) {
+  return x * y
+}
+multiply(2, 3.4)
+
+function divide(x, y) {
+  return x / y
+}
+divide(5.0, 2.5)
+
+
+
+
+
+ 
+function add5() {
+  number += 5
+}
+ 
+function divideBy3() {
+  number /= 3
+}
+
+function inc(n) {
+  n++;
+  return n;
+}
+
+function dec(n) {
+  n--;
+  return n;
+}
+
+function makeInt(n) {
+ parseInt('2', 10);
+  return ;
+
+}
+
+function preserveDecimal(n){
+  parseFloat('sldkjflksjf')
+  return n;
+}
+function makeInt(n) {
+  return  parseInt(n, 10);
+}
+
+
+function preserveDecimal(n){
+return parseFloat(n, 2.222)}
